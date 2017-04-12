fd05de866b0c0b10c53907bee44a609505d9ab38
Patricio Beltran <pbelsal@gmail.com>
2017-04-12 15:47:08 -0500

    add car functions

--- car.py
+++ car.py
@@ -17,9 +17,27 @@ class Car():
     def create_new_car_at_position(lane, speed, pos):
         return Car(c.COMPUTER_COLOR, lane, speed, pos)
 
+    @classmethod
+    def _calculate_global_position_with_direction(self, position, direction):
+        if direction == c.DIRECTION_DOWN:
+            return position
+        else:
+            pos = 0
+            if position == 0:
+                pos = c.HEIGHT
+            else:
+                pos = (-1 * (position - c.HEIGHT)%c.HEIGHT)
+            return pos - c.CAR_HEIGHT
+        
     def change_lanes(self, new_lane):
         self.lane = new_lane
     
+    def get_direction(self):
+        return self.lane.get_direction()
+
+    def get_position(self):
+        return self.position
+
     def distance_to_light(self):
         if not self.lane.has_light():
             return -1
@@ -49,7 +67,7 @@ class Car():
         return False
 
     def distance_to_pedestrian(self, pedestrian):
-        ped_pos = self._calculate_position_in_lane(pedestrian.get_position())
+        ped_pos = self._calculate_global_position_with_direction(pedestrian.get_position(), self.lane.get_direction())
         car_pos = self._calculate_position()
 
         if car_pos + c.CAR_HEIGHT <= ped_pos:
@@ -67,20 +85,57 @@ class Car():
         else:
             return car_x + c.CAR_WIDTH < pedestrian.get_x()
 
-    def _calculate_position(self):
-        return self._calculate_position_in_lane(self.position)
+    def get_speed(self):
+        return self.speed
+    
+    def distance_to_car(self, car):
+        if (not car.is_in_lane(self.lane)):
+            return -1
 
-    def _calculate_position_in_lane(self, position):
-        if self.lane.get_direction() == c.DIRECTION_DOWN:
-            return position
+        if self.position >= car.get_position() + c.CAR_HEIGHT:
+            return self.position - (car.get_position() + c.CAR_HEIGHT)
+        elif self.position + c.CAR_HEIGHT <= car.get_position():
+            return car.get_position() - (self.position + c.CAR_HEIGHT)
         else:
-            pos = 0
-            if position == 0:
-                pos = c.HEIGHT
-            else:
-                pos = (-1 * (position - c.HEIGHT)%c.HEIGHT)
-            return pos - c.CAR_HEIGHT
+            return 0
 
+    def is_blocking_lane(self, car, lane):
+        if (not car.is_in_lane(lane)):
+            return False
+        if (self.is_in_lane(lane)):
+            return False
+        
+        return self._crashing_positions(car)
+
+    def has_crashed(self, car):
+        if (car.is_in_lane(self.lane)):
+            return self._crashing_positions(car)
+        return False
+    
+    def get_relative_position(self, car):
+        if (not car.is_in_lane(self.lane)):
+            return c.POSITION_UNKNOWN
+
+        if self.position >= car.get_position() + c.CAR_HEIGHT:
+            return c.POSITION_BEHIND
+        elif self.position + c.CAR_HEIGHT <= car.get_position():
+            return c.POSITION_FRONT
+        return c.POSITION_COLLIDE
+
+    def is_in_lane(self, lane):
+        return self.lane is lane
+    
+    def _calculate_position(self):
+        return self._calculate_global_position_with_direction(self.position, self.lane.get_direction())
+
+    def _crashing_positions(self, car):
+        car_pos = self._calculate_position()
+        other_pos = car._calculate_position()
+        if (car_pos <= other_pos and car_pos + c.CAR_HEIGHT >= other_pos):
+            return True
+        elif (other_pos <= car_pos and other_pos + c.CAR_HEIGHT >= car_pos):
+            return True
+        return False
 
     def update(self):
         self.position = (self.position + self.speed) % c.HEIGHT
